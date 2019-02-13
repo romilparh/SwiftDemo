f93b730e46abea690f61f8cf9fbda14cfa00efdc
Romil Parhwal <romilparhwal007@gmail.com>
2019-02-13 13:21:43 -0500

    Initialise Project

---
+++ SwiftDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDemo
+//
+//  Created by Romil Parhwal on 2019-02-13.
+//  Copyright © 2019 Romil Parhwal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
